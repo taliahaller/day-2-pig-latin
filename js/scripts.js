ab480e0a9c9174be56574230d8189e2454b84a4e
Krystal Foster and Talia Haller <krystal@>
2018-08-15 15:04:54 -0700

    completed getting single words that start with a vowel to go to pig latin.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ var translated = 0;
 
 
 
-
 // User Interface Logic
 
 
@@ -15,74 +14,23 @@ $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
 
-    var userInside = $("input#userInput").val();
+    var userOriginal = $("input#userInput").val();
     var vowels = ["a", "e", "i", "o", "u"];
-    for (var i = 0; i < vowels.length; i++) {
+    var userInside = userOriginal.slice(0,1);
+
+      for (var i = 0; i < vowels.length; i++) {
       if (userInside === vowels[i]) {
         console.log(vowels[i]);
         console.log(userInside);
-        var result = userInside + "way";
+        var result = userOriginal + "way";
          console.log(result);
        $(".translated").text(result);
        break;
-     } else {
-       alert("Not ready for consonants!");
+     } else {userInside === vowels[i]
+
        // consonant use case
        // $(".translated").text(userInside);
      }
     }
   });
 });
-
-// //for (i = 0; i < userInside.length; i++) {
-//     for (j = 0;, j < vowels.length; j++) {
-//          if (userInside === vowels[j]) {
-
-
-//     }
-// }
-
-
-
-
-
-//     for (var i = 0; i < vowels.length; i++) {
-//        var vowel = vowels[i];
-//       var translate = [vowel];
-//       translated.push(translate);
-//
-//        var userInput = $("input#userInput").val();
-//        var result = userInput + "way";
-//
-//     if (userInput === translated) {
-//       $(".translated").text(result);
-//     } else {
-//       $(".translated").text(userInput);
-//     }
-// };
-// });
-// });
-
-
-
-// $(document).ready(function() {
-//   $("#pig-latin").submit(function(event) {
-//     event.preventDefault();
-//     // var userInput = $("input#userInput").val();
-//
-//     for (var i = 0; i < vowels.length; i++) {
-//        var vowel = vowels[i];
-//       var translate = [vowel];
-//       translated.push(translate);
-//
-//        var userInput = $("input#userInput").val();
-//        var result = userInput + "way";
-//
-//     if (userInput === translated) {
-//       $(".translated").text(result);
-//     } else {
-//       $(".translated").text(userInput);
-//     }
-// };
-// });
-// });
