432aecf5cd1fd5b06cdac9cbaaf4d78423d7a405
Krystal Foster and Talia Haller <krystal@>
2018-08-16 11:58:19 -0700

    complete Pig Latin exercise minus dealing with punctuation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // Business Logic
 
-var vowels = ["a", "e", "i", "o", "u"];
-var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
-
-var translated = 0;
+// var vowels = ["a", "e", "i", "o", "u"];
+// var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
+//
+// var translated = 0;
 
 
 
@@ -15,29 +15,68 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userOriginal = $("input#userInput").val().toLowerCase();
-    userOriginal = userOriginal.split(" ");
-    // console.log(userOriginal);
-    // var vowels = ["a", "e", "i", "o", "u"];
+    var plainSentence = userOriginal.split(" ");
     var pigLatin = [];
 
-    for(var j = 0; j < userOriginal.length; j++) {
-      var word = userOriginal[j];
-      var letter = word.slice(0,1);
+ for (var i = 0; i < plainSentence.length; i++) {
+   var checkWord = plainSentence[i];
 
-      loop2:
-        for (var i = 0; i < vowels.length; i++) {
-        if (letter === vowels[i]) {
-        var resultVowel = word + "way";
-             pigLatin.push(resultVowel);
-              console.log(pigLatin);
-                  // break loop2;
-          } else {
-            var resultConsonant = word + "ay";
-         pigLatin.push(resultConsonant);
+   if (/[^a-z'].*/i.test(checkWord)) {
+
+  loop1:
+     for (var k = 0; k < checkWord.length; k++) {
+       if (/.*[!,.]$/m.test(checkWord.charAt(k))) {
+         var beforePunctuation = checkWord.slice(0, k);
+         var afterPunctuation = checkWord.slice(k);
+         var pigTransform = beforePunctuation + "way" + afterPunctuation;
+         pigLatin.push(pigTransform);
+         console.log(beforePunctuation);
+         console.log(afterPunctuation);
+         break loop1;
+       } else {
+         console.log(checkWord);
+         pigLatin.push(checkWord);
+         break loop1;
        }
-         console.log(pigLatin);
-     $(".translated").text(pigLatin.join(" "));
-      }
      }
-   });
- });
+
+
+     // pigLatin.push(checkWord);
+   } else if (/^[aeiouy].*/i.test(checkWord)) {
+      var pigTransform = checkWord + "way";
+      pigLatin.push(pigTransform);
+
+   } else {
+
+     loop2:
+
+     for (var j = 0; j < checkWord.length; j++) {
+       if (/[aeiouy]+/i.test(checkWord.charAt(j))) {
+         if ((checkWord.charAt(j) == "u") && (checkWord.charAt(j-1) == "q")) {
+           var beforeVowel = checkWord.slice(0, j+1);
+           var afterVowel = checkWord.slice(j+1);
+           var pigTransform = afterVowel + beforeVowel + "ay";
+           pigLatin.push(pigTransform);
+           break loop2;
+         } else {
+           var beforeVowel = checkWord.slice(0, j);
+           var afterVowel = checkWord.slice(j);
+           var pigTransform = afterVowel + beforeVowel + "ay";
+           pigLatin.push(pigTransform);
+           break loop2;
+         }
+       }
+     }
+    }
+   }
+
+   pigLatin = pigLatin.join(" ");
+        pigLatin = pigLatin.toLowerCase();
+        pigLatin = pigLatin.replace(/^\w/, function (cap) {
+          return cap.toUpperCase();
+        });
+
+$(".translated").text(pigLatin);
+
+  });
+});
